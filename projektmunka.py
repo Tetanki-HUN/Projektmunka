bf764d94b0e6e1ac301f92f287cdded7f60725b2
Gulyás Zsolt Máté <85568371+Tetanki-HUN@users.noreply.github.com>
2022-06-07 17:57:19 +0200

    Add files via upload

---
+++ projektmunka.py
@@ -0,0 +1,80 @@
+BOARD_COLS = 7
+BOARD_ROWS = 6
+
+class Palya():
+    def __init__(self):
+        self.board = [[' ' for _ in range(BOARD_COLS)] for _ in range(BOARD_ROWS)]
+        self.turns = 0
+        self.last_move = [-1, -1] # [r, c]
+
+    def print_board(self):
+        print("\n")
+        for r in range(BOARD_COLS):
+            print(f"  ({r+1}) ", end="")
+        print("\n")
+
+'''
+Pályát majd kiírassuk
+'''
+
+    def turn(self, column):
+        for i in range(BOARD_ROWS-1, -1, -1):
+            if self.board[i][column] == ' ':
+                self.board[i][column] = self.which_turn()
+                self.last_move = [i, column]
+
+                self.turns += 1
+                return True
+
+        return False
+
+    def check_winner(self):
+        last_row = self.last_move[0]
+        last_col = self.last_move[1]
+        last_letter = self.board[last_row][last_col]
+
+        directions = [[[-1, 0], 0, True], 
+                      [[1, 0], 0, True], 
+                      [[0, -1], 0, True],
+                      [[0, 1], 0, True],
+                      [[-1, -1], 0, True],
+                      [[1, 1], 0, True],
+                      [[-1, 1], 0, True],
+                      [[1, -1], 0, True]]
+        
+        for a in range(4):
+            for d in directions:
+                r = last_row + (d[0][0] * (a+1))
+                c = last_col + (d[0][1] * (a+1))
+
+                if d[2] and self.in_bounds(r, c) and self.board[r][c] == last_letter:
+                    d[1] += 1
+                else:
+                    '''
+                    Nem keres ebbe az irányba
+                    '''
+                    d[2] = False
+
+        '''
+        Keressen 4-es sort
+        '''
+        for i in range(0, 7, 2):
+            if (directions[i][1] + directions[i+1][1] >= 3):
+                self.print_board()
+                print(f"{last_letter} a nyertes!")
+                return last_letter   
+
+        '''
+        Nem talált nyertest
+        '''
+        return False
+
+def play():
+    jatek = Palya()
+
+'''
+Játékszövegek ide kerülnek
+'''
+
+if __name__ == '__main__':
+    play()
