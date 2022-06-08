e0a005242d513e9a9a8af4757b6cfa46433c91a3
Marki-Bazsi <83362602+Marki-Bazsi@users.noreply.github.com>
2022-06-08 08:19:59 +0200

    Add files via upload

---
+++ projekt.py
@@ -0,0 +1,113 @@
+BOARD_COLS = 7
+BOARD_ROWS = 6
+
+class Palya():
+    def __init__(self):
+        self.board = [[' ' for _ in range(BOARD_COLS)] for _ in range(BOARD_ROWS)]
+        self.turns = 0
+        self.last_move = [-1, -1] # [r, c]
+
+    def print_board(self):
+        print("\n")
+        for r in range(BOARD_COLS):
+            print(f"  ({r+1}) ", end="")
+        print("\n")
+        
+        
+        for r in range(BOARD_ROWS):
+            print('|', end="")
+            for c in range(BOARD_COLS):
+                print(f"  {self.board[r][c]}  |", end="")
+            print("\n")
+
+        print(f"{'-' * 42}\n")
+
+
+    def which_turn(self):
+        players = ['X', 'O']
+        return players[self.turns % 2]
+    
+    def in_bounds(self, r, c):
+        return (r >= 0 and r < BOARD_ROWS and c >= 0 and c < BOARD_COLS)
+    def turn(self, column):
+        for i in range(BOARD_ROWS-1, -1, -1):
+            if self.board[i][column] == ' ':
+                self.board[i][column] = self.which_turn()
+                self.last_move = [i, column]
+
+                self.turns += 1
+                return True
+
+        return False
+
+
+    def which_turn(self):
+        players = ['X', 'O']
+        return players[self.turns % 2]
+    
+    def in_bounds(self, r, c):
+        return (r >= 0 and r < BOARD_ROWS and c >= 0 and c < BOARD_COLS)
+
+
+    def turn(self, column):
+        for i in range(BOARD_ROWS-1, -1, -1):
+            if self.board[i][column] == ' ':
+                self.board[i][column] = self.which_turn()
+                self.last_move = [i, column]
+
+                self.turns += 1
+                return True
+
+        return False
+
+    def check_winner(self):
+        last_row = self.last_move[0]
+        last_col = self.last_move[1]
+        last_letter = self.board[last_row][last_col]
+
+        directions = [[[-1, 0], 0, True], 
+                      [[1, 0], 0, True], 
+                      [[0, -1], 0, True],
+                      [[0, 1], 0, True],
+                      [[-1, -1], 0, True],
+                      [[1, 1], 0, True],
+                      [[-1, 1], 0, True],
+                      [[1, -1], 0, True]]
+        
+        for a in range(4):
+            for d in directions:
+                r = last_row + (d[0][0] * (a+1))
+                c = last_col + (d[0][1] * (a+1))
+
+                if d[2] and self.in_bounds(r, c) and self.board[r][c] == last_letter:
+                    d[1] += 1
+                else:
+                    '''
+                    Nem keres ebbe az irányba
+                    '''
+                    d[2] = False
+
+        '''
+        Keressen 4-es sort
+        '''
+        for i in range(0, 7, 2):
+            if (directions[i][1] + directions[i+1][1] >= 3):
+                self.print_board()
+                print(f"{last_letter} a nyertes!")
+                return last_letter   
+
+        '''
+        Nem talált nyertest
+        '''
+        return False
+
+def play():
+    jatek = Palya()
+
+'''
+Játékszövegek ide kerülnek
+'''
+
+if _name_ == '_main_':
+    play()
+Írás nekik: Zsolt Máté Gulyás
